f7e110ec0d1c7731f7b406c566afc54c3c9d0841
Yaroslav <workbox72@bk.ru>
2023-06-19 21:39:57 +0500

    add HW_Les_5

---
+++ HW_Les_5/task_1_to_4.swift
@@ -0,0 +1,93 @@
+// 3. Создайте протокол, в котором будут содержаться функции открытия и закрытия.
+protocol WorkProtocol {
+    func start()
+    func final()
+}
+
+protocol MenuProtocol {
+    var name: String { get }
+    var cost: Double { get }
+}
+
+// 4 - Написать расширение для класса пиццерии, в котором будет реализован протокол из пункта 3.
+extension Pizzeria: WorkProtocol {
+    func start() {
+        print("Start")
+    }
+    func final() {
+        print("Final")
+    }
+}
+struct Pizza: MenuProtocol{
+    enum PizzaType: String{
+        case quattroStagioni = "Quattro Stagioni"
+        case fruttiDiMare = "Frutti di Mare"
+        case braccioDiFerro = "Braccio di Ferro"
+        case prosciuttoEFunghi = "Prosciutto e Funghi"
+        case montanara = "Montanara"
+    }
+    enum Additives: String{
+        case mushrooms = "Mushrooms"
+        case meat = "Meat"
+        case cheese = "Cheese"
+        case spinach = "Spinach"
+        case seafood = "Seafood"
+        case pepperoni = "Pepperoni"
+        case tomato = "Tomato"
+        case anchovies = "Anchovies"
+        case pepper = "Pepper"
+    }
+    enum Dough: String{
+        case thick = "Thick"
+        case thin = "Thin"
+    }
+    var type: PizzaType
+    var name: String { type.rawValue + " pizza"}
+    let cost: Double
+    let dough: Dough
+    var additives: Additives
+    init(type: PizzaType, cost: Double) {
+        self.type = type
+        self.cost = cost
+    }
+}
+
+// 1.  Необходимо создать структуру картошки фри, в которой будет стоимость и размер картошки и сделать так, 
+// чтобы в классе пиццерии была одна переменная, в которую можно было бы класть и пиццу, и картошку фри..
+struct PotatoFrite: MenuProtocol{
+    enum PotatoFriteType: String {
+        case sweet = "Sweet"
+        case steak = "Steak"
+        case tornado = "Tornado"
+        case straw = "Straw"
+        case classic = "Classic"
+    }
+    var type: PotatoFriteType
+    var name: String { type.rawValue + " potato frite"}
+    let cost: Double
+    let size: Double
+    init(type: PotatoFriteType, cost: Double) {
+        self.type = type
+        self.cost = cost
+    }
+}
+
+class Pizzeria{
+    private var menu: [MenuProtocol] = []
+    init(menu: [MenuProtocol]) {
+        self.menu = menu
+    }
+    // 2 - Добавьте в класс пиццерии функцию, которая будет добавлять новую позицию в меню.
+    func add(_ new: MenuProtocol) {
+        menu.append(new)
+    }
+
+    // Добавить в класс кафе функцию, которая возвращает все позиции меню /// 2 часть
+    func get() -> [MenuProtocol]{
+        return menu
+    }
+
+    func getAvailable(cost: Double) -> [MenuProtocol]{ /// 2 часть
+        menuItems.filter{$0.price <= cost}
+    }
+}
